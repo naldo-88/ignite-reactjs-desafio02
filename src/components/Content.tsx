7b0fa87a8363bdc21033347aec0e845d50aa6414
Ednaldo Ferreira <naldo_a.f@hotmail.com>
2021-09-23 07:59:02 -0300

    Desafio Finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,71 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import { Header } from './Header';
+
+import { MovieCard } from './MovieCard';
+
+import { api } from '../services/api';
+
+import '../styles/content.scss';
+
+type ContentProps = {
+  selectedGenreId: number;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function Content(props: ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${props.selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${props.selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [props.selectedGenreId]);
+  
+  return(
+    <div className="container">
+      
+      <Header title={selectedGenre.title} />
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+type HeaderProps = {
+  title: string;
+}
+
+export function Header(props: HeaderProps) {
+  return (
+    <header>
+      <span className="category">Categoria:<span> {props.title}</span></span>
+    </header>
+  )
+}
